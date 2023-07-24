39d95d9fc45650e8bc7672907d2048b1521f43c6
spookyscaryyy <wbonner20@vt.edu>
2023-07-24 19:13:49 -0400

    added basic starter structure

---
+++ src/gbnts.h
@@ -0,0 +1,14 @@
+#ifndef _GBNTS_H
+#define _GBNTS_H
+
+/* Macros/Definitions */
+#define CLASS_NAME_MAX 10
+
+/* Data Structures */
+typedef struct {
+    char className[10];
+} CLASS_WORK_T;
+
+/* Globals */
+
+#endif
