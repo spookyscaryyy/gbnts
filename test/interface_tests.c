c6f956e8590eba89e738e6a8e9a8a35d04b4cf77
spookyscaryyy <wbonner20@vt.edu>
2023-08-02 08:52:04 -0400

    finished setting up testing framework

--- test/interface_tests.c
+++ test/interface_tests.c
@@ -1,5 +1,4 @@
 #include <check.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 #include "../src/scodes.h"
@@ -90,17 +89,3 @@ Suite* interface_suite(void)
     return s;
 }
 
-int main(void)
-{
-    int failed;
-    Suite *s;
-    SRunner *sr;
-
-    s = interface_suite();
-    sr = srunner_create(s);
-
-    srunner_run_all(sr, CK_NORMAL);
-    failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
-    return failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
-}
